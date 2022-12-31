535433265f7d68b7495a81da934ec208032a1781
garudkarshubham <garudkarshubham2989@gmail.com>
2022-12-31 16:42:13 +0530

    Input Validation: Trim Strings

--- utils/validators/patchProduct.js
+++ utils/validators/patchProduct.js
@@ -1,12 +1,12 @@
 const { check } = require('express-validator')
 
 const patchProductValidator = [
-	check('image').optional().isURL(),
+	check('image').optional().trim().isURL(),
 	check('price').optional().isInt({ min: 0 }),
 	check('quantity').optional().isInt({ min: 0 }),
-	check('description').optional().isString().isLength({ min: 300 }),
-	check('make').optional().isString().isLength({ min: 4, max: 16 }),
-	check('name').optional().isString().isLength({ min: 1 })
+	check('description').optional().trim().isString().isLength({ min: 300 }),
+	check('make').optional().trim().isString().isLength({ min: 4, max: 16 }),
+	check('name').optional().trim().isString().isLength({ min: 1 })
 ]
 
 module.exports = patchProductValidator
--- utils/validators/postProduct.js
+++ utils/validators/postProduct.js
@@ -1,12 +1,12 @@
 const { check } = require('express-validator')
 
 const postProductValidator = [
-	check('image').isURL(),
+	check('image').trim().isURL(),
 	check('price').isInt({ min: 0 }),
 	check('quantity').isInt({ min: 0 }),
-	check('description').isString().isLength({ min: 300 }),
-	check('make').isString().isLength({ min: 4, max: 16 }),
-	check('name').isString().isLength({ min: 1 })
+	check('description').trim().isString().isLength({ min: 300 }),
+	check('make').trim().isString().isLength({ min: 4, max: 16 }),
+	check('name').trim().isString().isLength({ min: 1 })
 ]
 
 module.exports = postProductValidator
